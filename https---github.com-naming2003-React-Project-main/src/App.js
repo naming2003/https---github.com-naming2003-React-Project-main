6ab4a14525ba9489fa031415cbce0cc9fade6fc3
naming11 <khunings9237@gmail.com>
2024-09-17 18:31:23 +0700

    first commit

---
+++ https---github.com-naming2003-React-Project-main/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import Register from './components/Register';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<SignIn/>} />
+        <Route exact path='/SignIn' element={<SignIn/>} />
+        <Route exact path='/SignUp' element={<SignUp/>}/>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
